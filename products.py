e8e12fed500f076a0d5542d511ec7da2eadbe231
Adolph Wei <log9527@gmail.com>
2020-07-15 23:34:38 +0800

    casting

--- products.py
+++ products.py
@@ -4,10 +4,11 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格：')
+	price = int(price)
 	products.append([name, price])	
 print(products)
 
 
 with open('products.csv', 'w') as f:
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')  # price is integer now, and should cast to string
